acd782203a30f8dc7ac12254d8980276480572f1
Gabriel Bouchard <33035583+Goutch@users.noreply.github.com>
2025-03-02 12:25:23 -0500

    menu sample

--- Samples/Menu/MenuScene.cpp
+++ Samples/Menu/MenuScene.cpp
@@ -1,1 +1,69 @@
-#include "UIScene.h"
+#include "MenuScene.h"
+
+void MenuScene::createResources()
+{
+	MeshInfo mesh_info{};
+	mesh_info.attribute_info_count = 1;
+	mesh_info.attribute_infos = &VERTEX_ATTRIBUTE_INFO_POSITION2D;
+	mesh_info.flags = MESH_FLAG_NONE;
+	rounded_rectangle_mesh = Resources::createMesh(mesh_info);
+
+	Geometry::createRoundedRectTriangleFan(*rounded_rectangle_mesh, 2.0, 2.0, 0.2, 4, VERTEX_FLAG_NONE, PIVOT_CENTER);
+
+	ShaderInfo shader_info{};
+	shader_info.path = "shaders/defaults/Position2D.vert";
+	shader_info.stage = SHADER_STAGE_VERTEX;
+	vertex_shader = Resources::createShader(shader_info);
+
+	shader_info.path = "shaders/defaults/Position.frag";
+	shader_info.stage = SHADER_STAGE_FRAGMENT;
+	fragment_shader = Resources::createShader(shader_info);
+
+
+	RasterizationPipelineInfo pipeline_info{};
+	pipeline_info.attribute_info_count = 1;
+	pipeline_info.attribute_infos = &VERTEX_ATTRIBUTE_INFO_POSITION3D;
+	pipeline_info.vertex_shader = vertex_shader;
+	pipeline_info.fragment_shader = fragment_shader;
+	pipeline_info.flags = RASTERIZATION_PIPELINE_FLAG_NONE;
+	pipeline_info.topology = VERTEX_TOPOLOGY_TRIANGLE_FAN;
+
+	fan_pipeline = Resources::createRasterizationPipeline(pipeline_info);
+
+	RasterizationPipelineInstanceInfo pipeline_instance_info{};
+	pipeline_instance_info.flags = RASTERIZATION_PIPELINE_INSTANCE_FLAG_NONE;
+	pipeline_instance_info.rasterization_pipeline = fan_pipeline;
+
+	fan_pipeline_instance = Resources::createRasterizationPipelineInstance({fan_pipeline});
+	vec4 material = vec4(1.0, 0.0, 0.0, 1.0);
+	fan_pipeline_instance->setUniform("material", &material);
+}
+
+void MenuScene::setupScene()
+{
+	Entity camera_entity = createEntity3D();
+	camera_entity.attach<Camera2D>();
+	setCameraEntity(camera_entity);
+
+	Entity rounded_rectangle_entity = createEntity3D();
+
+	MeshRenderer* rounded_rectangle_renderer = rounded_rectangle_entity.attach<MeshRenderer>();
+	rounded_rectangle_renderer->mesh = rounded_rectangle_mesh;
+	rounded_rectangle_renderer->pipeline_instance = fan_pipeline_instance;
+
+}
+
+MenuScene::MenuScene()
+{
+	createResources();
+	setupScene();
+}
+
+MenuScene::~MenuScene()
+{
+	delete rounded_rectangle_mesh;
+	delete vertex_shader;
+	delete fragment_shader;
+	delete fan_pipeline;
+	delete fan_pipeline_instance;
+}
--- Samples/Menu/MenuScene.h
+++ Samples/Menu/MenuScene.h
@@ -1,5 +1,17 @@
 #pragma once
 #include "HBE.h"
-class UIScene: public Scene {
 
+class MenuScene : public Scene
+{
+	Mesh* rounded_rectangle_mesh = nullptr;
+	RasterizationPipeline* fan_pipeline = nullptr;
+	RasterizationPipelineInstance* fan_pipeline_instance = nullptr;
+	Shader* vertex_shader = nullptr;
+	Shader* fragment_shader = nullptr;
+
+public:
+	void createResources();
+	void setupScene();
+	MenuScene();
+	~MenuScene() override;
 };
