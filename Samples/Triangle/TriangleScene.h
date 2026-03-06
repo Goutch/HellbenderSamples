e05d50d2c7888b0e044bf3bca3b2214a21988003
Gabriel <gaboutch@outlook.com>
2026-03-06 16:12:27 -0500

    simple triangle sample

--- Samples/Triangle/TriangleScene.h
+++ Samples/Triangle/TriangleScene.h
@@ -6,14 +6,17 @@ using namespace HBE;
 
 class TriangleScene : public Scene
 {
-    Mesh* triangle_mesh;
-
-    Shader* vertex_shader;
-    Shader* fragment_shader;
-    RasterizationPipeline* pipeline;
-    RasterizationPipelineInstance* pipeline_instance;
+    Mesh triangle_mesh;
+    Shader vertex_shader;
+    Shader fragment_shader;
+    RasterizationPipeline pipeline;
+    PipelineInstance pipeline_instance;
     Entity triangle_entity;
-
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
 public:
     TriangleScene()
     {
@@ -21,13 +24,29 @@ public:
         setupScene();
     }
 
-    ~TriangleScene()
+    void createResources()
     {
-        delete triangle_mesh;
-        delete vertex_shader;
-        delete fragment_shader;
-        delete pipeline;
-        delete pipeline_instance;
+        MeshInfo triangle_info{};
+        triangle_info.attribute_info_count = 1;
+        triangle_info.attribute_infos = &VERTEX_ATTRIBUTE_INFO_POSITION3D;
+        triangle_mesh.alloc(triangle_info);
+        triangle_mesh.setBuffer(0, vertices, 3);
+
+        fragment_shader.loadGLSL("shaders/defaults/Position.frag", SHADER_STAGE_FRAGMENT);
+        vertex_shader.loadGLSL("shaders/defaults/Position.vert", SHADER_STAGE_VERTEX);
+
+        RasterizationPipelineInfo pipeline_info{};
+        pipeline_info.attribute_info_count = 1;
+        pipeline_info.attribute_infos = &VERTEX_ATTRIBUTE_INFO_POSITION3D;
+        pipeline_info.vertex_shader = vertex_shader.getHandle();
+        pipeline_info.fragment_shader = fragment_shader.getHandle();
+        pipeline_info.flags = RASTERIZATION_PIPELINE_FLAG_NONE;
+
+        pipeline.alloc(pipeline_info);
+        pipeline.allocInstance(pipeline_instance);
+
+        vec4 color = {1, 0, 0, 1};
+        pipeline_instance.setUniform("material", &color);
     }
 
     void setupScene()
@@ -38,53 +57,7 @@ public:
 
         triangle_entity = createEntity3D();
         MeshRenderer* triangle_renderer = triangle_entity.attach<MeshRenderer>();
-        triangle_renderer->mesh = triangle_mesh;
-        triangle_renderer->pipeline_instance = pipeline_instance;
-    }
-
-    void createResources()
-    {
-        MeshInfo triangle_info{};
-        triangle_info.attribute_info_count = 1;
-        triangle_info.attribute_infos = &VERTEX_ATTRIBUTE_INFO_POSITION3D;
-
-        triangle_mesh = context.createMesh(triangle_info);
-        float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
-        };
-        triangle_mesh->setBuffer(0, vertices, 3);
-
-
-        ShaderInfo vertex_shader_info{};
-        ShaderInfo fragment_shader_info{};
-
-        vertex_shader_info.path = "shaders/defaults/Position.vert";
-        vertex_shader_info.stage = SHADER_STAGE_VERTEX;
-
-        fragment_shader_info.path = "shaders/defaults/Position.frag";
-        fragment_shader_info.stage = SHADER_STAGE_FRAGMENT;
-
-        vertex_shader = context.createShader(vertex_shader_info);
-        fragment_shader = context.createShader(fragment_shader_info);
-
-        RasterizationPipelineInfo pipeline_info{};
-        pipeline_info.attribute_info_count = 1;
-        pipeline_info.attribute_infos = &VERTEX_ATTRIBUTE_INFO_POSITION3D;
-        pipeline_info.vertex_shader = vertex_shader;
-        pipeline_info.fragment_shader = fragment_shader;
-        pipeline_info.flags = RASTERIZATION_PIPELINE_FLAG_NONE;
-
-        pipeline = context.createRasterizationPipeline(pipeline_info);
-
-        RasterizationPipelineInstanceInfo pipeline_instance_info{};
-        pipeline_instance_info.rasterization_pipeline = pipeline;
-        pipeline_instance_info.flags = RASTERIZATION_PIPELINE_INSTANCE_FLAG_NONE;
-
-        pipeline_instance = context.createRasterizationPipelineInstance(pipeline_instance_info);
-
-        vec4 color = {1, 0, 0, 1};
-        pipeline_instance->setUniform("material", &color);
+        triangle_renderer->mesh = triangle_mesh.getHandle();
+        triangle_renderer->pipeline_instance = pipeline_instance.getHandle();
     }
 };
